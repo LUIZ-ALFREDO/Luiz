4a18002d5b4afaf93b4f095e11d48936e84bea3b
LUIZ-ALFREDO <lalvieira@gmail.com>
2016-12-12 11:30:39 -0200

    dsfghjhg

--- models/Quadrado.js
+++ models/Quadrado.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var QuadradoSchema = new mongoose.Schema({
   nome: String,
   base: Number,
-  altura: Nuber
+  altura: Number
 });
 
 module.exports = mongoose.model('Quadrado', QuadradoSchema);
